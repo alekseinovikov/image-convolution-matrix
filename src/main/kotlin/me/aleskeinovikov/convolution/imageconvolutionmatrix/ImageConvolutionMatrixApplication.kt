2cda9548a814ee7c854b205f2f14573797bbef7b
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2021-11-01 15:18:45 +0200

    Added first view

--- src/main/kotlin/me/aleskeinovikov/convolution/imageconvolutionmatrix/ImageConvolutionMatrixApplication.kt
+++ src/main/kotlin/me/aleskeinovikov/convolution/imageconvolutionmatrix/ImageConvolutionMatrixApplication.kt
@@ -1,11 +1,36 @@
 package me.aleskeinovikov.convolution.imageconvolutionmatrix
 
+import javafx.application.Application
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.context.ConfigurableApplicationContext
+import tornadofx.App
+import tornadofx.DIContainer
+import tornadofx.FX
+import kotlin.reflect.KClass
 
 @SpringBootApplication
-class ImageConvolutionMatrixApplication
+class ImageConvolutionMatrixApplication : App(MainView::class) {
+
+	private lateinit var context: ConfigurableApplicationContext
+
+	override fun init() {
+		this.context = SpringApplication.run(this.javaClass)
+		context.autowireCapableBeanFactory.autowireBean(this)
+
+		FX.dicontainer = object : DIContainer {
+			override fun <T : Any> getInstance(type: KClass<T>): T = context.getBean(type.java)
+			override fun <T : Any> getInstance(type: KClass<T>, name: String): T = context.getBean(name, type.java)
+		}
+	}
+
+	override fun stop() {
+		super.stop()
+		context.close()
+	}
+
+}
 
 fun main(args: Array<String>) {
-	runApplication<ImageConvolutionMatrixApplication>(*args)
+	Application.launch(ImageConvolutionMatrixApplication::class.java, *args)
 }
---
+++ src/main/kotlin/me/aleskeinovikov/convolution/imageconvolutionmatrix/MainView.kt
@@ -0,0 +1,11 @@
+package me.aleskeinovikov.convolution.imageconvolutionmatrix
+
+import tornadofx.View
+import tornadofx.label
+import tornadofx.vbox
+
+class MainView : View("Image Convolution Matrix") {
+    override val root = vbox {
+        label(text = "Hello! World!")
+    }
+}
