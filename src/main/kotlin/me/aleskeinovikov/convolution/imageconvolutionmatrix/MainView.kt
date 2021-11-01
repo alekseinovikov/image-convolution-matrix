8f29d54478b70fa481ba10aaf53c11d73ad486ec
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2021-11-01 15:39:29 +0200

    Added first view

--- src/main/kotlin/me/aleskeinovikov/convolution/imageconvolutionmatrix/MainView.kt
+++ src/main/kotlin/me/aleskeinovikov/convolution/imageconvolutionmatrix/MainView.kt
@@ -1,11 +1,39 @@
 package me.aleskeinovikov.convolution.imageconvolutionmatrix
 
-import tornadofx.View
-import tornadofx.label
-import tornadofx.vbox
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import javafx.stage.FileChooser
+import tornadofx.*
 
 class MainView : View("Image Convolution Matrix") {
+    private val fileChooser = FileChooser().also {
+        it.title = "Open image file"
+        it.extensionFilters.addAll(
+            FileChooser.ExtensionFilter("Images", "*.jpg", "*.jpeg", "*.png"),
+        )
+    }
+
     override val root = vbox {
-        label(text = "Hello! World!")
+        hbox {
+            val imageView = imageview {
+            }
+            button("Open image") {
+                setOnMouseClicked {
+                    fileChooser.showOpenDialog(this@MainView.currentWindow)?.let {
+                        val url = it.toURI().toURL()
+                        imageView.image = Image(url.toString())
+                        imageView.autoScale()
+                    }
+                }
+            }
+        }
+
+        hbox {
+
+        }
+    }
+
+    private fun ImageView.autoScale() {
+        this.fitWidth = 400.0
     }
 }
